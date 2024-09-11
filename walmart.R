1b9742b23b7fd117be6739481baba4a403cf435a
gdynis2 <157057316+gdynis2@users.noreply.github.com>
2024-09-11 12:24:31 -0500

    Add files via upload

--- walmart.R
+++ walmart.R
@@ -5,10 +5,10 @@ library(readr)
 library(dplyr)
 library(reshape2)
 
-df_cal <- read.csv('calendar.csv', stringsAsFactors = FALSE)
+df_cal <- read.csv('econ491/calendar.csv', stringsAsFactors = FALSE)
 df_cal$date = as.character(df_cal$date)
-df_sales_train <- read.csv('sales_train_validation.csv', stringsAsFactors = FALSE)
-df_prices <- read.csv('sell_prices.csv', stringsAsFactors = FALSE)
+df_sales_train <- read.csv('econ491/sales_train_validation.csv', stringsAsFactors = FALSE)
+df_prices <- read.csv('econ491/sell_prices.csv', stringsAsFactors = FALSE)
 
 start_date <- as.Date("2011-01-29")
 num_days <- 1913
@@ -22,11 +22,24 @@ names(df_melted_sales)[names(df_melted_sales) == "d"] <- "date"
 
 df_train <- merge(df_melted_sales, df_cal, by = 'date', all.x = TRUE)
 
-colSums(is.na(df_train))
+df_prices$id <- paste(df_prices$item_id, df_prices$store_id, sep = "_")
+df_prices <- df_prices[, !(names(df_prices) %in% c('item_id', 'store_id'))]
+
+df_train$id <- paste(df_train$item_id, df_train$store_id, sep = "_")
+
+df_train <- merge(df_train, df_prices, by = c('id', 'wm_yr_wk'), all.x = TRUE)
+
+df_train$event_name_1[is.na(df_train$event_name_1)] <- 'None'
+df_train$event_type_1[is.na(df_train$event_type_1)] <- 'None'
+df_train$event_name_2[is.na(df_train$event_name_2)] <- 'None'
+df_train$event_type_2[is.na(df_train$event_type_2)] <- 'None'
+
+
+
+
+
 
 View(df_melted_sales)
 View(df_cal)
 View(df_train)
-
-colnames(df_sales_train)
-list_id_vars
+View(df_prices)
