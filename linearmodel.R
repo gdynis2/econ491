b15d2beeeb59c1c4d77d57a5570751e9d3791257
Arhan Menon <92775836+arhanmenon@users.noreply.github.com>
2024-10-18 16:27:12 -0500

    Update linearmodel.R

--- linearmodel.R
+++ linearmodel.R
@@ -127,3 +127,50 @@ ggplot(plot_df, aes(x = date, y = errors)) +
   labs(title = "Line Plot of Squared Errors", x = "Date", y = "Squared Error") +
   theme_minimal()
 
+df_WI = df_summary %>% filter(state_id == 'WI')
+
+df_WI <- df_WI %>%
+  mutate(Monday = ifelse(weekday == "Monday", 1, 0),
+         Tuesday = ifelse(weekday == "Tuesday", 1, 0),
+         Wednesday = ifelse(weekday == "Wednesday", 1, 0),
+         Thursday = ifelse(weekday == "Thursday", 1, 0),
+         Friday = ifelse(weekday == "Friday", 1, 0),
+         Saturday = ifelse(weekday == "Saturday", 1, 0),
+         Sunday = ifelse(weekday == "Sunday", 1, 0))
+
+df_WI <- df_WI %>%
+  mutate(event = ifelse(event_name_1 == "None", 0, 1))
+
+train_df = head(df_WI, 1913 - 28)
+test_df = tail(df_WI, 28)
+
+print(dim(train_df))
+print(dim(test_df))
+
+lm_model_wi <- lm(total_sales ~ mean_sell_price +
+                    Monday + Tuesday + Wednesday + Thursday 
+                  + Friday + Saturday + snap_WI, train_df)
+summary(lm_model_wi)
+
+features = c("date", "mean_sell_price", "Monday", "Tuesday", "Wednesday", 
+             "Thursday", "Friday", "Saturday", "Sunday", "snap_WI")
+
+actual_y = test_df$total_sales
+test_values <- test_df[, features]
+
+predictions = as.numeric(predict(lm_model_wi, test_values))
+
+mse = mean((actual_y - predictions)^2, na.rm=TRUE)
+rmse = sqrt(mse)
+print(rmse)
+
+squared_errors = (actual_y - predictions)^2
+errors = (actual_y - predictions)
+
+plot_df = data.frame(date = as.Date(test_df$date), errors = squared_errors)
+
+ggplot(plot_df, aes(x = date, y = errors)) +
+  geom_line(color = "blue") +
+  labs(title = "Line Plot of Squared Errors", x = "Date", y = "Squared Error") +
+  theme_minimal()
+
